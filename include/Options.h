e51a5a33b4c2682c7475dbe5e4dae4c6ae384932
Fredprost <Frederic.Prost@imag.fr>
2013-12-11 10:21:31 +0100

    Update Options.h

--- include/Options.h
+++ include/Options.h
@@ -30,20 +30,23 @@
 #define DEFAULT_MAX_LINES 8
 #define DEFAULT_PLAY_FOR WHITE
 #define DEFAULT_VERBOSE_LEVEL 0
-#define DEFAULT_HASHMAP_SIZE 4096
-#define DEFAULT_PLAYFOR_MOVETIME_ 2500
-#define DEFAULT_PLAYAGAINST_MOVETIME_ 2000
-#define DEFAULT_OUTPUT_FILE ""
+#define DEFAULT_HASHMAP_SIZE 2024
+#define DEFAULT_PLAYFOR_MOVETIME_ 5 
+#define DEFAULT_PLAYAGAINST_MOVETIME_ 20 
+#define DEFAULT_OUTPUT_FILE "oracleout.txt"
 #define DEFAULT_INPUT_FILE ""
 #define DEFAULT_STARTPOS "startpos"
 #define DEFAULT_ENGINE "stockfish"
-#define DEFAULT_PATH "/usr/local/bin"
+#define DEFAULT_PATH "./."
 #define DEFAULT_TRESHOLD 100
 #define DEFAULT_MATE_EQUIV 100000
 #define DEFAULT_THREADS 4
 
 #define TIMEOUT_READY 5
 #define MOVES_DISPLAYED 5
+
+#define DEFAULT_PROBE 5
+#define DEFAULT_SYZYPATH "/Users/guest/Desktop/echecsprostmehdi/chessfinder/lstsyzygysf/tb/src"
 
 using namespace std;
 
